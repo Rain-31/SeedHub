773a88a030021ab43d1be0fad47c3411aa89d1c8
Rain-31 <51219764+Rain-31@users.noreply.github.com>
2025-12-05 18:36:09 +0800

    feat: update SeedReam 4.5 model

--- src/App.vue
+++ src/App.vue
@@ -133,8 +133,9 @@
                     v-model="form.model"
                     class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-500"
                   >
-                    <option value="doubao-seedream-4-0-250828">Doubao SeedDream 4.0</option>
-                    <option value="seedream-4-0-250828">SeedDream 4.0</option>
+                    <option value="doubao-seedream-4-0-250828">Doubao SeedReam 4.0</option>
+                    <option value="seedream-4-0-250828">SeedReam 4.0</option>
+                    <option value="seedream-4-5-251128">SeedReam 4.5</option>
                   </select>
                 </div>
 
